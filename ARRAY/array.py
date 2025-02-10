2c1b5c39321f1c7c40ab164ab2f6016791175a50
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-02-10 19:12:58 +0530

    Create array.py

---
+++ ARRAY/array.py
@@ -0,0 +1,15 @@
+# write a program to array by taking input from user .
+
+# Taking input for the size of the array
+size = int(input("Size of the array is: ")) 
+
+# Initializing an empty array
+arr = []
+
+# Taking input for array elements
+for i in range(size):
+    value = int(input(f"Enter element {i + 1}: "))
+    arr.append(value)
+
+# Printing the final array
+print(f"The {size}-dimensioned array is: {arr}")
