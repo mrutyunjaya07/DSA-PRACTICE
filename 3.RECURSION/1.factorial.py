d248ebbd354b02137f081d4a92ec957fe78c254b
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-03-27 23:04:04 +0530

    update

---
+++ 3.RECURSION/1.factorial.py
@@ -0,0 +1,16 @@
+# Function definition
+def factorial(num):
+    # Base case
+    if num == 0 or num == 1:
+        return 1
+    elif num < 0:
+        return "Invalid input !!"
+    else:
+        return num * factorial(num - 1)  # Recursive function call
+
+# User input
+num = int(input("Enter a positive number: "))
+
+# Function call and printing the result
+result = factorial(num)
+print("Factorial of the number is:", result)
