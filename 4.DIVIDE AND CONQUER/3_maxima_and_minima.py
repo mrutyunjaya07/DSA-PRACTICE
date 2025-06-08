8b5942ef1da781297950f70a5a3e09db8e2c6e82
mrutyunjaya07 <mjmohanty3058@gmail.com>
2025-06-08 19:28:37 +0530

    Update 3_maxima_and_minima.py

--- 4.DIVIDE AND CONQUER/3_maxima_and_minima.py
+++ 4.DIVIDE AND CONQUER/3_maxima_and_minima.py
@@ -1,5 +1,6 @@
 # find minima and maxima from a list
 
+#define the funcction
 def findMaximaAndMinima(arr,i,j):
 
     if i ==j:    # base case
@@ -15,12 +16,14 @@ def findMaximaAndMinima(arr,i,j):
             maximum=arr[i]
 
     else:
-
+        #divide and conquer
         mid = i+(j-i)//2
 
+        #recursive calls
         minl,maxl=findMaximaAndMinima(arr,i,mid)
         minr,maxr=findMaximaAndMinima(arr,mid+1,j)
 
+        #combine
         if minl<minr:
             minimum=minl
         else:
@@ -32,6 +35,9 @@ def findMaximaAndMinima(arr,i,j):
             maximum=maxr
     return minimum,maximum
 
+#input array
 arr=[23,6,98,887,34,0,56,-9]
+#function call
 minimum,maximum=findMaximaAndMinima(arr,0,len(arr)-1)
+#print statement
 print("Minimum valuee :",minimum,"Maximum valuee:",maximum)
