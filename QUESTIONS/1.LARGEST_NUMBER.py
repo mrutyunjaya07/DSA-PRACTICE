5b184cb722ffcc9e7f22d75fee8f6c5e16138c68
mrutyunjaya07 <mjmohanty3058@gmail.com>
2026-01-10 19:46:00 +0530

    add

--- QUESTIONS/1.LARGEST_NUMBER.py
+++ QUESTIONS/1.LARGEST_NUMBER.py
@@ -8,3 +8,16 @@ def largestNumber(numbers):
         if number > largest:
             largest = number
     return largest
+
+
+# Find the smallest number in a list of numbers
+def smallestNumber(numbers):
+
+    if not numbers: 
+        return None  # Return None if the list is empty
+    
+    smallest = numbers[0]
+    for number in numbers:
+        if number < smallest:
+            smallest = number
+    return smallest
--- QUESTIONS/2.SECOND_LARGEST.py
+++ QUESTIONS/2.SECOND_LARGEST.py
@@ -14,6 +14,24 @@ def secondLargest(numbers):
 
     return second if second != float('-inf') else None # Return None if there is no second largest number 
 
+def secondSmallest(numbers):
+    if len(numbers) < 2:
+        return None  # Return None if there are less than 2 numbers
+
+    first = second = float('inf') #initialize to positive infinity
+    for number in numbers:
+        if number < first:
+            second = first
+            first = number
+        elif first < number < second:
+            second = number
+
+    return second if second != float('inf') else None # Return None if there is no second smallest number
+
+
+
+
+
 # Example usage:
 numbers = [3, 5, 2, 9, 7]
 result = secondLargest(numbers)
